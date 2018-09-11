2fc2622921467826c493e1f1f9152be03d656d7f
Aditya Dixit <adityadixit06@gmail.com>
2018-09-11 09:40:08 +0530

    Added WeatherForecast component functionality

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,36 +1,49 @@
 import React from "react";
 import _ from "lodash";
-
+import './WeatherForecast.css';
 
 class WeatherForecast extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			weatherData: {}
-		};
+		}
 	}
 
-
 	componentDidMount() {
-		//fetch('https://api.myjson.com/bins/r2b3o')
-		fetch('https://api.myjson.com/bins/eux78')
-		//fetch('http://api.openweathermap.org/data/2.5/forecast?id=1275339&APPID=1f703b39afd8359290cc27e826b45021')
+		fetch('http://api.openweathermap.org/data/2.5/forecast?id=1275339&APPID=1f703b39afd8359290cc27e826b45021')
 		.then(res => res.json())
 		.then(data => this.setState({ weatherData: data }))
 	}
 
 	render() {
-		let ab = _.get(this.state.weatherData, ['list']);
-		let count = 40;
+		let weather = _.get(this.state.weatherData, ['list']);
 		return(
 			<div>
-				<h1> Maximum Temperature {_.get(this.state.weatherData, [ 'city', 'name']) }</h1>
-				<ul> 
-					{ ab && ab.map((tp) =>	<li key={count--}> {tp.dt} </li> )}
-				 </ul>
+				<h1 className="heading">5 day weather report for Mumbai</h1>
+				<div className="weather-data-container">
+					<ul className="weather-item-list" style={{fontWeight: "bold"}}>
+						<li key={"date"}>Date </li>
+						<li key={"temp"}>Temp</li>
+						<li key={"pre"}>Pressure</li>
+						<li key={"hum"}>Humidity</li>
+						<li key={"fore"}>Forecast</li>
+						<li key={"wind"}>Wind Speed</li>
+					</ul>
+				{ weather && weather.map((entry,index) =>	
+					<ul className="weather-item-list" key={index}>
+						<li key={"date"+ index}> {entry.dt_txt} </li>
+						<li key={"temp"+ index}>{(entry.main.temp - 273.15).toFixed(1)} &deg;C </li>
+						<li key={"pre"+ index}> {parseInt(entry.main.pressure)} hpa</li>
+						<li key={"hum"+ index}>{(entry.main.humidity)} %</li>
+						<li key={"fore"+ index}> {entry.weather[0].description} </li>
+						<li key={"wind"+ index}> {entry.wind.speed} m/s</li>
+					</ul>
+			 )} </div>
 			</div>
 		)
 	}
 }
+					
 
 export default WeatherForecast;
---
+++ src/components/WeatherForecast.test.js
@@ -0,0 +1,17 @@
+import  WeatherForecast from "./WeatherForecast";
+import React from "react";
+import { mount, shallow } from "enzyme";
+
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+configure({ adapter: new Adapter() });
+
+test('testing app heading h1', () => {
+	const forecast =  mount(<WeatherForecast />);
+	expect(forecast.find('.heading').text()).toEqual('5 day weather report')
+})
+
+
+test('There are 40 data-points', () => {
+	const forecast = mount(<WeatherForecast />);
+})
